4d6a44ef2009db20b05ae03fa925437b9af779d3
Patrick Santos <patrickbarnabe@github.com>
2020-07-29 10:45:29 -0300

    2.0

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 import "./menu.css"
 import Button from "./components/ButtonLink";
@@ -6,11 +7,11 @@ import Button from "./components/ButtonLink";
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Logo"/>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
